dfe52af56a04e0da5b683c7f95afb23dba74725a
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-18 18:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if(s1.length === 0 || s2.length === 0){
+		return -1;
+	}
+	let s1lower = s1.toLowerCase();
+	let s2lower = s2.toLowerCase();
+
+	let index = s1lower.indexOf(s2lower)
+	return index;
+	  
 }
 
 // Please do not change the code below
